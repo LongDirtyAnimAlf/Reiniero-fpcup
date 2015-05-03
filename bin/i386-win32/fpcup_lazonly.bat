250b3c8c7e11be9b1d1fc17164c99a7fd02b65af
LongDirtyAnimAlf <LongDirtyAnimAlf@gmail.com>
2015-05-03 18:43:45 +0200

    Correction of some typos

---
+++ bin/i386-win32/fpcup_lazonly.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+REM #####################################################
+REM               fpcup for windows
+REM #####################################################
+
+ECHO.
+ECHO ====================================================
+ECHO   Fpcup with trunk and defaults; Lazarus only
+ECHO ====================================================
+ECHO.
+
+if EXIST .\fpcup.exe (
+fpcup.exe --fpcURL="trunk" --lazURL="trunk" --skip="fpc,FPCCrossWin32-64"--verbose
+)
+
+ECHO.
+ECHO ====================================================
+ECHO   Fpcup with trunk and defaults ready; Lazarus only
+ECHO ====================================================
+ECHO.
---
+++ bin/i386-win32/fpcup_native.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+REM ###############################################
+REM               fpcup for windows
+REM ###############################################
+
+ECHO.
+ECHO ==============================================
+ECHO   Fpcup with trunk and defaults
+ECHO ==============================================
+ECHO.
+
+if EXIST .\fpcup.exe (
+fpcup.exe --fpcURL="trunk" --lazURL="trunk" --verbose
+)
+
+ECHO.
+ECHO ==============================================
+ECHO   Fpcup with trunk and defaults ready
+ECHO ==============================================
+ECHO.
